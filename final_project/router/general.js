3aa5db0bd1810c527bd782b468d41b4b7f857e9d
31240990-droid <31240990@vupune.ac.in>
2026-09-03 20:15:45 +0530

    Add book retrieval functions using axios

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,1 +1,20 @@
 
+const axios = require('axios');
+const BASE_URL = 'http://localhost:5000';
+async function getAllBooks() {
+  const res = await axios.get(`${BASE_URL}/books`);
+  return res.data;
+}
+function getBookByISBN(isbn) {
+  return new Promise((resolve, reject) => {
+    axios.get(`${BASE_URL}/isbn/${isbn}`).then(r=>resolve(r.data)).catch(reject);
+  });
+}
+async function getBooksByAuthor(author) {
+  const res = await axios.get(`${BASE_URL}/author/${author}`);
+  return res.data;
+}
+function getBooksByTitle(title) {
+  return axios.get(`${BASE_URL}/title/${title}`).then(r=>r.data);
+}
+module.exports = { getAllBooks, getBookByISBN, getBooksByAuthor, getBooksByTitle };
